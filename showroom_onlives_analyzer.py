9950fb7b0133b9f70224a3fafb027ff91588f842
natukin1978 <natukin1978@hotmail.com>
2025-07-22 22:24:57 +0900

    We have addressed the case where main_name does not exist.

--- showroom_onlives_analyzer.py
+++ showroom_onlives_analyzer.py
@@ -5,7 +5,7 @@ class ShowroomOnlivesAnalyzer:
     def get_live(self, main_name: str) -> dict[str, any] | None:
         for onlive in self.onlives["onlives"]:
             for live in onlive["lives"]:
-                if live["main_name"] == main_name:
+                if "main_name" in live and live["main_name"] == main_name:
                     return live
         return None
 
