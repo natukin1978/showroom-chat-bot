9f07449ff62786875e9f82d6e91d50d98fb38bbe
natukin1978 <natukin1978@hotmail.com>
2025-08-16 08:58:11 +0900

    Replaced with a convenience function.

--- showroom_onlives_analyzer.py
+++ showroom_onlives_analyzer.py
@@ -1,3 +1,6 @@
+from dict_helper import get_first_non_none_value
+
+
 class ShowroomOnlivesAnalyzer:
     def __init__(self):
         self.onlives = {}
@@ -5,7 +8,7 @@ class ShowroomOnlivesAnalyzer:
     def get_live(self, main_name: str) -> dict[str, any] | None:
         for onlive in self.onlives["onlives"]:
             for live in onlive["lives"]:
-                if "main_name" in live and live["main_name"] == main_name:
+                if get_first_non_none_value(live, ["main_name"]) == main_name:
                     return live
         return None
 
